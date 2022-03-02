395016dbf84fd751705d2a69fcf9a14fc22f1159
kalyani <kalyani.y@thundersoft.com>
2022-03-02 21:04:03 +0530

    march02

---
+++ funrectanglecr.c
@@ -0,0 +1,19 @@
+// to fing area of rectangle using functions call reference
+#include<stdio.h>
+int find_area(int*,int*);
+int main()
+{
+     int l,b;
+    printf("enter lenght and bredth of the rectangle");
+    scanf("%d%d",&l,&b);
+    find_area(&l,&b);
+    
+}
+int find_area(int* l,int* b)
+
+{
+    
+   int area=*l**b;
+    printf("area of the rectangle is %d",area);
+    return 0;
+}
